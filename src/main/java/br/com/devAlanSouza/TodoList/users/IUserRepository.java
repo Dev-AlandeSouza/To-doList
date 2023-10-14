fcae3c54655488364511e64a9c9315296055727f
Alan de Souza <alaan.correia.08@gmail.com>
2023-10-14 17:07:27 -0300

    Update IUser Repository

--- src/main/java/br/com/devAlanSouza/TodoList/users/IUserRepository.java
+++ src/main/java/br/com/devAlanSouza/TodoList/users/IUserRepository.java
@@ -1,10 +1,12 @@
 package br.com.devAlanSouza.TodoList.users;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+@Repository
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
 
-
-public interface IUserRepository extends JpaRepository<UserModel, UUID>{
-    UserModel findByUsername(String username);   
+    UserModel findByUsername(String username);
 }
